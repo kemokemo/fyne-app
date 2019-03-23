d14a58383fb23a32c98ccfda8f3389f144651f93
KemoKemo <t2wonderland@gmail.com>
2019-03-23 19:02:20 +0900

    feat: add first app.

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fyne.io/fyne/app"
+	"fyne.io/fyne/widget"
+)
+
+func main() {
+	a := app.New()
+
+	w := a.NewWindow("Hello")
+	w.SetContent(widget.NewVBox(
+		widget.NewLabel("Hello Fyne!"),
+		widget.NewButton("Quit", func() {
+			a.Quit()
+		}),
+	))
+
+	w.ShowAndRun()
+}
