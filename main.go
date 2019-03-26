c6f111c666ea563819c30d021b950c3f632ab925
KemoKemo <t2wonderland@gmail.com>
2019-03-26 23:07:10 +0900

    feat: add widgets with fake save function. #1

--- main.go
+++ main.go
@@ -1,20 +1,30 @@
 package main
 
 import (
+	"log"
+
+	"fyne.io/fyne"
 	"fyne.io/fyne/app"
+	"fyne.io/fyne/layout"
 	"fyne.io/fyne/widget"
 )
 
 func main() {
 	a := app.New()
 
-	w := a.NewWindow("Hello")
-	w.SetContent(widget.NewVBox(
-		widget.NewLabel("Hello Fyne!"),
-		widget.NewButton("Quit", func() {
-			a.Quit()
-		}),
-	))
+	w := a.NewWindow("text pad")
+	w.Resize(fyne.NewSize(640, 480))
+	le := widget.NewMultiLineEntry()
+	le.SetPlaceHolder("Please input your words!")
+	bt := widget.NewButton("Save", func() {
+		log.Println("The save button was pressed!")
+	})
+
+	box := fyne.NewContainerWithLayout(
+		layout.NewBorderLayout(nil, bt, nil, nil),
+		le, bt,
+	)
+	w.SetContent(box)
 
 	w.ShowAndRun()
 }
